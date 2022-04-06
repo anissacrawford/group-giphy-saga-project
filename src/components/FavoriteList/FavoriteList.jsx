31ad138d0d4efa7e766e1dbecd704fde2e66baef
Anissa Crawford <anissacrawford@gmail.com>
2022-04-06 16:44:54 -0500

    favorite get

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,6 +1,35 @@
-function FavoriteList () {
+import axios from 'axios';
+import React from 'react';
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import FavoriteItem from '../FavoriteItem/FavoriteItem';
+
+function FavoriteList() {
+
+    const favorites = useSelector(store => store.favoriteReducer);
+    const dispatch = useDispatch();
+
+    const getFavorites = () => {
+        axios.get('/api/favorite')
+            .then(response => {
+                console.log(response.data);
+                // console.log(response.data?.data[0].images.original.url);
+                // setSearchResults(response.data.data);
+                dispatch({ type: 'GET_FAVORITE', payload: response.data })
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+
+    useEffect(() => {
+        getFavorites();
+    }, []);
+
     return (
         <>
+            <div>{favorites.map((favorite) => {
+                return ( <FavoriteItem key={favorite.id} favorite={favorite} /> )})} </div>
+
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -5,29 +5,46 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
+
+//get
+function* getFavorite(){
+    try { 
+        let response = yield axios.get('/api/favorite');
+        console.log(response.data);
+        //dispatch is put only in index 
+        yield put({ type:'ADD_FAVORITE', payload: response.data });
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 //saga middleware
 const sagaMiddleware = createSagaMiddleware(); 
 
 //rootSaga
 function* rootSaga(){
-
+    yield takeEvery('GET_FAVORITE', getFavorite);
 }
 
-//reducer
-const getGiphy = (state = 0, action) => {
-    console.log('testing get');
-    return state;
-}
+//reducer 
+const favoriteReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'ADD_FAVORITE':
+        return action.payload
+      default:
+        return state;
+    }
+  };
 
 //store
 const store = createStore(
     combineReducers({
-        getGiphy
+        favoriteReducer
     }),
     applyMiddleware(sagaMiddleware, logger)
-)
-
+);
 
 //pass rootSaga to middleware 
 sagaMiddleware.run(rootSaga);
